ec57f353236fb01fe8ce4ff52caf06511b8bb97b
Afolabi Akorede <braincommect0@gmail.com>
2022-05-19 14:05:01 +0100

    Make the popup responsive by clicking

--- script.js
+++ script.js
@@ -1,73 +1,45 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll('.nav_link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-}));
+document.querySelectorAll(".nav_link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
 
 // POPUP WINDOW SECTION
 
-// var modal = document.getElementById('simpleModal');
-// var modalBtn = document.getElementById('modalBtn');
-// var closeBtn = document.getElementsByClassName('closeBtn')[0];
+//getting modal openning button
+var modalBtns = document.querySelectorAll('.modal-open');
 
-// modalBtn.addEventListener('click', openModal);
-
-// closeBtn.addEventListener('click', closeModal);
-
-// function openModal(){
-//   modal.style.display = 'block';
-// }
-
-// function closeModal(){
-//   modal.style.display = 'none';
-// }
-
-//getting modal opening buttons
-const btns = document.querySelectorAll('.see-project');
-const close_btns = document.querySelectorAll('.closeBtn')
-
-btns.forEach(function(btn) {
-  btn.onclick = function() {
+modalBtns.forEach(function(btn){
+  btn.onclick = function(){
     var modal = btn.getAttribute('data-modal');
 
-    document.getElementById(modal).style.display = 'block'
+    document.getElementById(modal).style.display = 'block';
   };
 });
 
-close_btns.forEach(function(btn) {
-  btn.onclick = function() {
-    var modal = btn.getAttribute('data-modal');
+// clicking on the span close Btn
 
-    document.getElementsByClassName('.closeBtn').style.display = 'none'
+var closeBtns = document.querySelectorAll('.closeBtn');
+closeBtns.forEach(function(btn){
+  btn.onclick = function(){
+    var modal = btn.closest('.modal').style.display = 'none';
   };
 });
 
-// close_btns.forEach((btn) => {
-//   btn.addEventListener('click', () => {
-//     document.querySelector(btn.dataset.target).classList.remove('active');
-//   });
-// });
+// clicking outside the box
 
-// var closeBtn = document.getElementsByClassName('closeBtn');
-
-// closeBtn.addEventListener('click', closeModal);
-
-// function closeModal(){
-//   modal.style.display = 'none';
-// }
-
-
-// var closeBtn = document.querySelectorAll('.closeBtn');
-
-// closeBtn.forEach(function(btn) {
-//   btn.onclick = function() {
-//     var closeBtn = (btn.closest('.closeBtn').style.display = 'none');
-//   };
-// });
+window.onclick = function(e){
+  if(e.target.className === 'modal') {
+    e.target.style.display = 'none';
+  }
+};
