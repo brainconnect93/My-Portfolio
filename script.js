19b8759c34809e6db6998afa146510b9da7264b2
Afolabi Akorede <braincommect0@gmail.com>
2022-05-19 14:59:38 +0100

    fix linters

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav_link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
-
+document.querySelectorAll('.nav_link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
 
 // POPUP WINDOW SECTION
 
-//getting modal openning button
-var modalBtns = document.querySelectorAll('.modal-open');
+// getting modal openning button
+const modalBtns = document.querySelectorAll('.modal-open');
 
-modalBtns.forEach(function(btn){
-  btn.onclick = function(){
-    var modal = btn.getAttribute('data-modal');
+modalBtns.forEach((btn) => {
+  btn.onclick = function () {
+    const modal = btn.getAttribute('data-modal');
 
     document.getElementById(modal).style.display = 'block';
   };
@@ -29,17 +26,22 @@ modalBtns.forEach(function(btn){
 
 // clicking on the span close Btn
 
-var closeBtns = document.querySelectorAll('.closeBtn');
-closeBtns.forEach(function(btn){
-  btn.onclick = function(){
-    var modal = btn.closest('.modal').style.display = 'none';
+const closeBtns = document.querySelectorAll('.closeBtn');
+closeBtns.forEach((btn) => {
+  btn.onclick = function () {
+
+    // const modal = btn.getAttribute('.modal');
+    
+    var close = btn.closest('.modal').style.display = 'none';
+
+    // document.getElementsByClassName('.closeBtn').style.display = 'none';
   };
 });
 
 // clicking outside the box
 
-window.onclick = function(e){
-  if(e.target.className === 'modal') {
+window.onclick = function(e) {
+  if (e.target.className === 'modal') {
     e.target.style.display = 'none';
   }
 };
