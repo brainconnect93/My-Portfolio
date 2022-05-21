9e6ddad30e38e1864b05f8be92c678fe04be0404
Afolabi Akorede <braincommect0@gmail.com>
2022-05-21 11:18:58 +0100

    bug-exercise

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  hamburger.toggle.remove('hide');
+  navMenu.toggle.remove('hide');
 });
 
 document.querySelectorAll('.nav_link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  hamburger.toggle.remove('hide');
+  navMenu.toggle.remove('hide');
 }));
